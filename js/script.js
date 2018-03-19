d93dc33d3166c05aa9387af8bfc65f06d61e4478
Kaila Santiago <ksantiago.student@hsfi.us>
2018-03-19 22:07:01 +0000

    search and random button working

--- js/script.js
+++ js/script.js
@@ -1,10 +1,54 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
 $(document).ready(function(){
-  
-  
-  
-  
+      function giphyURLWithSearchTerm(searchTerm) {
+        var searchTerm = $("input").val();
+        var gifUrl = "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC";
+        return gifUrl;
+    
+    }
+    
+    function appendImageToBody(srcURL) {
+        $("#result").html('<img src=' + srcURL + '>');
+    }
+    
+    function callGiphyAPIWithSearchTerm(searchTerm) {
+        $.ajax({
+          url: giphyURLWithSearchTerm(searchTerm),
+          method: "GET",
+          success: function(response) {
+            var random = Math.ceil(Math.random() * response.data.length);
+            var url = response.data[random].images.original.url;
+            appendImageToBody(url);
+          },
+        }); 
+    }
+
+    $("#search").click(function() {
+        callGiphyAPIWithSearchTerm();
+        $("#srch-term").val("");
+    });
+    
+    
+    function randomGiphyURL() {
+            var randomUrl = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC";
+            return randomUrl;
+        }
+        function callRandomGiphyURL() {
+            $.ajax({
+              url: randomGiphyURL(),
+              method: "GET",
+              success: function(response) {
+                 console.log(response);
+                   var url = response.data.images.original.url;
+                   appendImageToBody(url);
+            },
+        }); 
+    }
+    
+    $("#randomButton").click(function() {
+    callRandomGiphyURL();
+    });
+    
 });
